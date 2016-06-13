0d00067e441728c2b7cbb72039efb439a09c0806
waquer <waquer@hotmail.com>
2016-06-13 09:29:18 +0800

    调整格式

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,14 +1,12 @@
 var rules = [
-    { "p" : "http://ajax.googleapis.com",    "t" : "http://ajax.useso.com" }
-    ,{ "p" : "http://fonts.googleapis.com",  "t" : "http://fonts.useso.com" }
-    ,{ "p" : "https://ajax.googleapis.com",  "t" : "https://ajax.lug.ustc.edu.cn" }
-    ,{ "p" : "https://fonts.googleapis.com", "t" : "https://fonts.lug.ustc.edu.cn" }
-    ,{ "p" : "themes.googleusercontent.com", "t" : "google-themes.lug.ustc.edu.cn" }
-    ,{ "p" : "fonts.gstatic.com",            "t" : "fonts-gstatic.lug.ustc.edu.cn" }
-    ,{ "p" : "http://(www|\\d).gravatar.com","t" : "http://gravatar.duoshuo.com" }
-
+    { "p" : "http://ajax.googleapis.com",    "t" : "http://ajax.useso.com" },
+    { "p" : "http://fonts.googleapis.com",  "t" : "http://fonts.useso.com" },
+    { "p" : "https://ajax.googleapis.com",  "t" : "https://ajax.lug.ustc.edu.cn" },
+    { "p" : "https://fonts.googleapis.com", "t" : "https://fonts.lug.ustc.edu.cn" },
+    { "p" : "themes.googleusercontent.com", "t" : "google-themes.lug.ustc.edu.cn" },
+    { "p" : "fonts.gstatic.com",            "t" : "fonts-gstatic.lug.ustc.edu.cn" },
+    { "p" : "http://(www|\\d).gravatar.com","t" : "http://gravatar.duoshuo.com" }
 ];
-
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
